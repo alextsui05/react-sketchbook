94a3e9b85df30a33b83cd915a091e9516b16fb94
Alex Tsui <alextsui@pm.me>
2026-02-11 19:18:48 +0000

    Remove refresh token manipulating code

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -6,14 +6,9 @@ export const Route = createFileRoute('/login')({
 });
 
 function RouteComponent() {
-  const [accessToken, setAccessToken] = useState(
-    localStorage.getItem('accessToken'),
-  );
-  const [refreshToken, setRefreshToken] = useState(
-    localStorage.getItem('refreshToken'),
-  );
+  const [accessToken, setAccessToken] = useState(null);
 
-  const login = async (event: React.FormEvent<HTMLFormElement>) => {
+  const login: React.SubmitEventHandler<HTMLFormElement> = async (event) => {
     // const url = 'https://a.atsui.click/auth/login';
     const url = 'http://localhost:3000/login';
     event.preventDefault();
@@ -32,9 +27,6 @@ function RouteComponent() {
       const data = await response.json();
       console.log(data);
       setAccessToken(data.access_token);
-      setRefreshToken(data.refresh_token);
-      localStorage.setItem('accessToken', data.access_token);
-      localStorage.setItem('refreshToken', data.refresh_token);
     } catch (error) {
       console.error(error);
     }
@@ -44,26 +36,18 @@ function RouteComponent() {
     const url = 'http://localhost:3000/refresh';
     const response = await fetch(url, {
       method: 'POST',
-      body: JSON.stringify({
-        refresh_token: refreshToken,
-      }),
       headers: {
         'Content-Type': 'application/json',
       },
+      credentials: 'include',
     });
     const data = await response.json();
     console.log(data);
     setAccessToken(data.access_token);
-    setRefreshToken(data.refresh_token);
-    localStorage.setItem('accessToken', data.access_token);
-    localStorage.setItem('refreshToken', data.refresh_token);
   };
 
   const logout = () => {
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('refreshToken');
     setAccessToken(null);
-    setRefreshToken(null);
   };
 
   return (
@@ -72,7 +56,6 @@ function RouteComponent() {
         <>
           <p>Logged in</p>
           <p>Access token: {accessToken}</p>
-          <p>Refresh token: {refreshToken}</p>
           <button onClick={logout}>Logout</button>
           <button onClick={refresh}>Refresh</button>
         </>
